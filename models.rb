944a8dd28adcaee3a47090e1df3596c38ba90b09
Greg Borenstein <greg@mfdz.com>
2011-04-29 12:00:35 -0400

    add models and views

---
+++ models.rb
@@ -0,0 +1,35 @@
+require 'rubygems'
+require 'dm-core'
+
+module Magick
+  class Image
+    attr_accessor :col_img
+    def composite_column!(img, col, mycol)
+      @col_img ||= Image.new(1, img.rows)
+      @col_img.composite! img, -col, 0, ReplaceCompositeOp
+      composite! @col_img, mycol, 0, ReplaceCompositeOp
+    end
+  end
+end
+
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || {:adapter => 'yaml', :path => 'db'})
+    
+class Grid
+  include DataMapper::Resource
+  property :id,         Serial    
+  property :name,       String    
+
+  has n, :images
+end    
+
+class Image
+
+  include DataMapper::Resource
+
+  property :id,         Serial    
+  property :created_at, DateTime
+  
+  belongs_to :grid
+  
+end
